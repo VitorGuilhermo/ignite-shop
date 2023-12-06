a1dbf332b42d222807c899e912574181af59a430
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-12-06 14:48:34 -0300

    add: stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,26 @@
-import Head from 'next/head'
+import { styled } from "../styles/index"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <Button>
+      <span>aa</span>
+      Enviar
+    </Button>
   )
 }
