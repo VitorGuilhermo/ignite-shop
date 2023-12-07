dca6f557b436ad97f8be26108d2e9a6be612a221
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-12-07 17:18:40 -0300

    add: stripe checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -11,11 +11,16 @@ interface ProductProps {
         imageUrl: string
         price: string
         description: string
+        defaultPriceId: string
     }
 }
 
 
 export default function Product({ product }: ProductProps) {
+    function handleBuyButton() {
+        console.log(product.defaultPriceId);
+    }
+
     return (
         <ProductContainer>
             <ImageContainer>
@@ -28,7 +33,7 @@ export default function Product({ product }: ProductProps) {
 
                 <p>{product.description}</p>
 
-                <button>
+                <button onClick={handleBuyButton}>
                     Comprar agora
                 </button>
             </ProductDetails>
@@ -65,7 +70,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
                     style: 'currency',
                     currency: 'BRL'
                 }).format(price.unit_amount / 100),
-                description: product.description
+                description: product.description,
+                defaultPriceId: price.id
             }
         },
         revalidate: 60 * 60 * 1 // 1 hours
